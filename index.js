50118081587a8a6112566a78729c330010ddc8d5
Kabir Virji <kabirvirji@gmail.com>
2017-03-09 22:41:29 -0500

    cleaned up, functionality complete

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const prettyjson = require('prettyjson');
-const config = require('./config.json');
 const SpotifyWebApi = require('spotify-web-api-node');
 const fs = require('fs');
 const got = require('got');
-const playlist = require('./createPlaylist.js')
-const spotifyApi = new SpotifyWebApi({
-  clientId : config.spotifyid,
-  clientSecret : config.spotifysecret,
-  redirectUri : 'http://localhost/'
-});
-function prettyConsole(data) {
-  console.log(prettyjson.render(data));
-}
-'use strict';
 const meow = require('meow');
-// const foo = require('.');
+'use strict';
 
-// node index.js -a "Kendrick Lamar" -n 20 -r 3
-// 20 song playlist with 
+const singlespotify = async function singlespotify(inputs, flags) {
 
-const foo = async function foo(inputs, flags) {
+	// -a "Kanye West"
 	const artistName = flags['a'];
+	// -c path/to/config.json
+	const configFile = flags['c'];
+
+	// get bearer token from path to config file
+	var configJSON = JSON.parse(require('fs').readFileSync(configFile, 'utf8'));
+
 	var tracks = [];
 	var artists = [];
+
+	const spotifyApi = new SpotifyWebApi({
+	  clientId : configJSON.spotifyid,
+	  clientSecret : configJSON.spotifysecret
+	});
 
 	// get artist URI
 	const artistSearch = await spotifyApi.searchArtists(artistName);
@@ -46,15 +44,19 @@ const foo = async function foo(inputs, flags) {
 
 
 	// for (i<0;i<3;i++){
+	// 	console.log(i);
 	// 	let current = await spotifyApi.getArtistTopTracks(artists[i], 'CA');
 	// 	current = current.body.tracks;
+	// 	console.log('current');
 	// 	for (j=0;j<3;j++){
+	// 		console.log(j);
 	// 		tracks.push(current[j].uri);
 	// 	}
 	// }
+	// exit();
 
 	// add related artist top songs to tracks array
-	
+
 	let artistOne = await spotifyApi.getArtistTopTracks(artists[0], 'CA');
 	artistOne = artistOne.body.tracks;
 	for (i=0;i<3;i++){
@@ -73,115 +75,62 @@ const foo = async function foo(inputs, flags) {
 		tracks.push(artistThree[i].uri);
 	}
 
-
-
-
-
-	console.log(tracks);
-	console.log(artists);
-
 	// create a playlist
 	var options = {
 	  json: true, 
 	  headers: {
 	    'Content-type': 'application/json',
-	    'Authorization' : `Bearer ${config.bearer}`,
+	    'Authorization' : `Bearer ${configJSON.bearer}`,
 	    'Accept' : 'application/json'
 	  },
 	  body: JSON.stringify({ name: `${artistName}: singlespotify`, public : true})
 	};
 	console.log(options);
 
-	got.post(`https://api.spotify.com/v1/users/${config.username}/playlists`, options)
+	got.post(`https://api.spotify.com/v1/users/${configJSON.username}/playlists`, options)
 	  .then(response => {
 	    const playlistID = response.body.id;
-	    console.log(playlistID);
 
-	// add tracks to playlist
-	function populatePlaylist (id, uris) {
-		var url = `https://api.spotify.com/v1/users/${config.username}/playlists/${id}/tracks?uris=${uris}`
-		var options = {
-		  json: true, 
-		  headers: {
-		    'Content-type': 'application/json',
-		    'Authorization' : `Bearer ${config.bearer}`,
-		  }
-		};
-		got.post(url, options)
-		  .then(response => {
-		    console.log(response.body);
-		  })
-		  .catch(err => { console.log(err) 
-		  });
-	}
+			// add tracks to playlist
+			function populatePlaylist (id, uris) {
+				var url = `https://api.spotify.com/v1/users/${configJSON.username}/playlists/${id}/tracks?uris=${uris}`
+				var options = {
+				  json: true, 
+				  headers: {
+				    'Content-type': 'application/json',
+				    'Authorization' : `Bearer ${configJSON.bearer}`,
+				  }
+				};
+				got.post(url, options)
+				  .then(response => {
+				    console.log(response.body);
+				  })
+				  .catch(err => { console.log(err) 
+				  });
+			}
 
-	populatePlaylist(playlistID, tracks);
-
-
-
-
-
-
+			populatePlaylist(playlistID, tracks);
 
 	  })
-	  .catch(err => { console.log(err) 
+
+	  .catch(err => { console.log('Please update your bearer token in your config.json')
+
 	  });
-
-
-
-
-
-
-
-
-		// spotifyApi.getArtistTopTracks(artistURI, 'CA')
-		//   .then(function(data) {
-		//     console.log(data.body);
-		//     }, function(err) {
-		//     console.log('Something went wrong!', err);
-		//   });
-
-
-
-		// spotifyApi.getArtistRelatedArtists(artistURI)
-		//   .then(function(data) {
-		//     //console.log(data.body);
-		//   }, function(err) {
-		//     console.log(err);
-		//   });
-
-
 
 }
 
-
-
 const cli = meow(`
     Usage
-      $ foo <input>
-
-    Options
-      --rainbow, -r  Include a rainbow
+      $ singlespotify -a "<artist_name>" -c /path/to/config.json
 
     Examples
       $ foo unicorns --rainbow
       🌈 unicorns 🌈
 `, {
     alias: {
-        a: 'artist'
+        a: 'artist',
+        c: 'configFile'
     }
 });
-/*
-{
-    input: ['unicorns'],
-    flags: {rainbow: true},
-    ...
-}
-*/
 
-foo(cli.input[0], cli.flags);
-
-// node index.js -a "some artist name"
-
-
-
+singlespotify(cli.input[0], cli.flags);
