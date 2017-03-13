0a7bc2d06911c1f13869b67d9870cda9fd879107
Kabir Virji <kabirvirji@gmail.com>
2017-03-13 18:35:17 -0400

    ✨

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const got = require('got');
 const meow = require('meow');
 const chalk = require('chalk');
 const ora = require('ora');
+const inquirer = require('inquirer');
+const Conf = require('conf');
 const spinner = ora('Loading ...');
 
-const singlespotify = async function singlespotify(inputs, flags) {
 
-	// ora loading spinner
-	spinner.start();
+const singlespotify = async function singlespotify(inputs, flags) {
 
 	// -a "Kanye West"
 	const artistName = flags['a'];
@@ -25,123 +25,133 @@ const singlespotify = async function singlespotify(inputs, flags) {
 		return
 	}
 
-	// -c path/to/config.json
-	const configFile = flags['c'];
+	inquirer.prompt([
+		  {
+		    type: 'input',
+		    message: 'Enter your Spotify username',
+		    name: 'username'
+		  },
+		  {
+		    type: 'password',
+		    message: 'Enter your Spotify bearer token',
+		    name: 'bearer'
+		  }
+	]).then(async function (answers) {
 
-	// get bearer token from path to config file
-	try {
-		var configJSON = JSON.parse(require('fs').readFileSync(configFile, 'utf8'));
-	}
-	catch(err) {
-		spinner.fail('Failed');
-		console.log(chalk.red(`
-	Oops! That wasn't a valid config path. Try again please!
+		JSON.stringify(answers, null, '  ');
 
-	See https://github.com/kabirvirji/singlespotify#usage for more information
-	`))
-		return
-	}
+		const config = new Conf();
 
-	var tracks = [];
-	var artists = [];
+		// sway ain't got the
+		config.set(answers);
 
-	const spotifyApi = new SpotifyWebApi();
+		// ora loading spinner
+		spinner.start();
 
-	// get artist URI
-	const artistSearch = await spotifyApi.searchArtists(artistName);
-	// error check for invalid search
-	if (artistSearch.body.artists.items[0] === undefined) {
-		spinner.fail('Failed');
-		console.log(chalk.red(`
+		var tracks = [];
+		var artists = [];
+
+		const spotifyApi = new SpotifyWebApi();
+
+		// get artist URI
+		const artistSearch = await spotifyApi.searchArtists(artistName);
+		// error check for invalid search
+		if (artistSearch.body.artists.items[0] === undefined) {
+			spinner.fail('Failed');
+			console.log(chalk.red(`
 
 	Oops! That search didn't work. Try again please!
-	`))
-		return
-	}
-	let artistURI = artistSearch.body.artists.items[0].uri;
-	artistURI = artistURI.slice(15);
+		`))
+			return
+		}
+		let artistURI = artistSearch.body.artists.items[0].uri;
+		artistURI = artistURI.slice(15);
 
-	// get artist top tracks
-	let artistTopTracks = await spotifyApi.getArtistTopTracks(artistURI, 'CA');
-	artistTopTracks = artistTopTracks.body.tracks;
-	for (let artistTrack of artistTopTracks) {
-		tracks.push(artistTrack.uri);
-	}
+		// get artist top tracks
+		let artistTopTracks = await spotifyApi.getArtistTopTracks(artistURI, 'CA');
+		artistTopTracks = artistTopTracks.body.tracks;
+		for (let artistTrack of artistTopTracks) {
+			tracks.push(artistTrack.uri);
+		}
 
-	// get three related artists
-	let relatedArtists = await spotifyApi.getArtistRelatedArtists(artistURI);
-	relatedArtists = relatedArtists.body.artists;
-	for (var i=0;i<3;i++){
-		var currentArtist = relatedArtists[i].uri;
-		artists.push(currentArtist.slice(15));
-	}
+		// get three related artists
+		let relatedArtists = await spotifyApi.getArtistRelatedArtists(artistURI);
+		relatedArtists = relatedArtists.body.artists;
+		for (var i=0;i<3;i++){
+			var currentArtist = relatedArtists[i].uri;
+			artists.push(currentArtist.slice(15));
+		}
 
-	// add related artists top songs to tracks array
-	let artistOne = await spotifyApi.getArtistTopTracks(artists[0], 'CA');
-	artistOne = artistOne.body.tracks;
-	for (var i=0;i<3;i++){
-		tracks.push(artistOne[i].uri);
-	}
-	let artistTwo = await spotifyApi.getArtistTopTracks(artists[1], 'CA');
-	artistTwo = artistTwo.body.tracks;
-	for (var i=0;i<3;i++){
-		tracks.push(artistTwo[i].uri);
-	}
-	let artistThree = await spotifyApi.getArtistTopTracks(artists[2], 'CA');
-	artistThree = artistThree.body.tracks;
-	for (var i=0;i<3;i++){
-		tracks.push(artistThree[i].uri);
-	}
+		// add related artists top songs to tracks array
+		let artistOne = await spotifyApi.getArtistTopTracks(artists[0], 'CA');
+		artistOne = artistOne.body.tracks;
+		for (var i=0;i<3;i++){
+			tracks.push(artistOne[i].uri);
+		}
+		let artistTwo = await spotifyApi.getArtistTopTracks(artists[1], 'CA');
+		artistTwo = artistTwo.body.tracks;
+		for (var i=0;i<3;i++){
+			tracks.push(artistTwo[i].uri);
+		}
+		let artistThree = await spotifyApi.getArtistTopTracks(artists[2], 'CA');
+		artistThree = artistThree.body.tracks;
+		for (var i=0;i<3;i++){
+			tracks.push(artistThree[i].uri);
+		}
 
-	// create an empty public playlist
-	var options = {
-	  json: true, 
-	  headers: {
-	    'Content-type': 'application/json',
-	    'Authorization' : `Bearer ${configJSON.bearer}`,
-	    'Accept' : 'application/json'
-	  },
-	  body: JSON.stringify({ name: `${artistName}: singlespotify`, public : true})
-	};
+		// create an empty public playlist
+		var options = {
+		  json: true, 
+		  headers: {
+		    'Content-type': 'application/json',
+		    'Authorization' : `Bearer ${config.get('bearer')}`,
+		    'Accept' : 'application/json'
+		  },
+		  body: JSON.stringify({ name: `${artistName}: singlespotify`, public : true})
+		};
 
-	got.post(`https://api.spotify.com/v1/users/${configJSON.username}/playlists`, options)
-	  .then(response => {
-	    const playlistID = response.body.id;
+		got.post(`https://api.spotify.com/v1/users/${config.get('username')}/playlists`, options)
+		  .then(response => {
+		    const playlistID = response.body.id;
 
-			// function to add tracks to playlist
-			function populatePlaylist (id, uris) {
-				var url = `https://api.spotify.com/v1/users/${configJSON.username}/playlists/${id}/tracks?uris=${uris}`
-				var options = {
-				  json: true, 
-				  headers: {
-				    'Content-type': 'application/json',
-				    'Authorization' : `Bearer ${configJSON.bearer}`,
-				  }
-				};
-				got.post(url, options)
-				  .then(response => {
-				  	spinner.succeed('Success!');
-				    console.log(chalk.green(`
+				// function to add tracks to playlist
+				function populatePlaylist (id, uris) {
+					var url = `https://api.spotify.com/v1/users/${config.get('username')}/playlists/${id}/tracks?uris=${uris}`
+					var options = {
+					  json: true, 
+					  headers: {
+					    'Content-type': 'application/json',
+					    'Authorization' : `Bearer ${config.get('bearer')}`,
+					  }
+					};
+					got.post(url, options)
+					  .then(response => {
+					  	spinner.succeed('Success!');
+					    console.log(chalk.green(`
 	Your playlist is ready! 
 	It's called "${artistName}: singlespotify"`));
-				  })
-				  .catch(err => { 
-				  	spinner.fail('Failed');
-				  	console.log(chalk.red("There was an error adding songs to the playlist. Please try again.")); 
-				  });
-			}
+					  })
+					  .catch(err => { 
+					  	spinner.fail('Failed');
+					  	console.log(chalk.red("There was an error adding songs to the playlist. Please try again.")); 
+					  });
+				}
 
-			populatePlaylist(playlistID, tracks);
+				populatePlaylist(playlistID, tracks);
 
-	  })
+		  })
 
-	  .catch(err => { spinner.fail('Failed');
-	  	console.log(chalk.red(`
-	ERROR: Please update your bearer token in your config.json
+		  .catch(err => { spinner.fail('Failed');
+		  	console.log(chalk.red(`
+	ERROR: Incorrect username or bearer token
+
+	You might need to update your bearer token
 
 	Generate a new one at https://developer.spotify.com/web-api/console/post-playlists/`));
 
-	  });
+		  });
+
+		 });
 
 }
 
@@ -149,10 +159,14 @@ spinner.stop();
 
 const cli = meow(chalk.cyan(`
     Usage
-      $ singlespotify --artist [-a] "artist_name" --config [-c] /path/to/config.json
+      $ singlespotify --artist [-a] "artist_name"
+      ? Enter your Spotify username <username>
+      ? Enter your Spotify bearer token <bearer>
 
     Example
-      $ singlespotify -a "Kanye West" -c /Users/kabirvirji/config.json
+      $ singlespotify -a "Kanye West"
+      ? Enter your Spotify username kabirvirji
+      ? Enter your Spotify bearer token ************************************************************
 
     For more information visit https://github.com/kabirvirji/singlespotify
 
